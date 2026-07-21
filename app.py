a57c3fbd2d25632e5a6d0a1009434c3859e9b325
Fatma Fauziah <fatmafauziahpam@gmail.com>
2026-07-21 21:09:08 +0700

    Fix logi redirect

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ with app.app_context():
 # ==========================
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return redirect(url_for("login"))
 
 
 # ==========================
